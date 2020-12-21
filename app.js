c5391bf88d5af8e685910bc79a1b5ba132e20e3a
Fred <frediek@gmx.de>
2020-12-21 02:35:58 +0100

    mobile fixes 3

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     snowFlake.style.fontSize = fontsize + 'px';
 
     //Set Snowflake horizontal position
-    let horizontal = Math.floor(Math.random() * 90);
+    let horizontal = Math.floor(Math.random() * 100);
     snowFlakeWrapper.style.left = horizontal + '%';
 
     snowFlakeWrapper.style.top = '-55px';
